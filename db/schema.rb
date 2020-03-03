107b29a1e2b60997411fa96a8fe2d9ea276b28f9
rosemarie betrong <rmbt.lab@gmail.com>
2020-03-02 21:56:15 -0500

    added Task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_020440) do
+ActiveRecord::Schema.define(version: 2020_03_03_025425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,14 @@ ActiveRecord::Schema.define(version: 2020_02_28_020440) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "path"
     t.index ["user_id"], name: "index_services_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
