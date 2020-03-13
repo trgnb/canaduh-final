17be76b61375a137d7fdbb103b9239be5c54bfe4
rosemarie betrong <rmbt.lab@gmail.com>
2020-03-12 20:13:25 -0400

    added Achived attribute to milestones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_044425) do
+ActiveRecord::Schema.define(version: 2020_03_13_001053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_03_12_044425) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "completion"
+    t.boolean "achieved"
     t.index ["user_id"], name: "index_milestones_on_user_id"
   end
 
