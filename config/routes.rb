52c30f929ebafb4e6b69c89d2d9ffc2714b5a38c
Belis Turegun <trgnb6@gmail.com>
2020-03-24 10:42:28 -0400

    added users controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
    get '/users/sign_out' => 'devise/sessions#destroy'
 end
 
+  resources :users, only: [:show, :edit, :update]
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :services do
