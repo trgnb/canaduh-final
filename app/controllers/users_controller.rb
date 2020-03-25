81cf242302e8a37e4f52246e001609dd29026d6d
Belis Turegun <trgnb6@gmail.com>
2020-03-24 22:22:16 -0400

    added onboarding form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
   def update
+    @users = User.all
     @user = current_user
     @user.update_attributes(user_params)
+    @services = Service.all
+    redirect_to services_path(current_user, query: current_user.path_type)
   end
 
   def edit
     @user = current_user
   end
 
+  def show
+    @users = User.all
+    @user = current_user
+    @user.update_attributes(user_params)
+  end
+
   private
 
   def user_params
-    params.require(:users).permit(:first_name, :last_name, :email, :user_type)
+    params.permit(:user_type, :path_type, :id)
   end
 end
