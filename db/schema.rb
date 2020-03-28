34c87e7a2a3e5c3ed0dc070488e2daeee1f87752
rosemarie betrong <rmbt.lab@gmail.com>
2020-03-28 13:00:33 -0400

    added Address Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_031221) do
+ActiveRecord::Schema.define(version: 2020_03_28_165956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
