4334326471a6f36de40c21482736b806764501a6
rosemarie betrong <rmbt.lab@gmail.com>
2020-03-28 13:02:59 -0400

    Removed coordinates columns in Rides

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_165956) do
+ActiveRecord::Schema.define(version: 2020_03_28_170119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,8 +129,6 @@ ActiveRecord::Schema.define(version: 2020_03_28_165956) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_rides_on_user_id"
   end
 
