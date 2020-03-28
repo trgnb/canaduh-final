4468b85cc1c01f62f67617940430189d3e40751f
Belis Turegun <trgnb6@gmail.com>
2020-03-28 14:36:57 -0400

    precompile line added

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,5 @@ module CanaduhMarketplace
     config.generators.system_tests = nil
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.assets.initialize_on_precompile = false
-
   end
 end
