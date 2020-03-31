9173813a83bcd24f4288bdbb9310cc5b29565634
Belis Turegun <trgnb6@gmail.com>
2020-03-30 22:55:13 -0400

    changes to the timeline - not done

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,19 +1,22 @@
 var completed = document.querySelectorAll(".complete");
 var toggleButton = document.getElementById("toggleButton");
 var hey = document.querySelector(".card-task");
-var checkBox = document.querySelector(".far fa-square");
+var checkBox = document.querySelectorAll("i#status");
+var tasks = document.querySelectorAll("#CSQ")
+var taskDetails = document.querySelector(".task-details #milestone").innerText;
+var journeyDetails = document.querySelector(".timestamp .author").innerText;
 
 // to-do:
 // add milestone tasks under recommended tasks
 // select them unique id
 //once they're added to task list, add a click event, which will toggle "complete" in the progress bar with matching id
 
-
+console.log(checkBox);
+console.log(tasks);
 console.log(hey);
+console.log(taskDetails);
+console.log(journeyDetails);
 
-function add(){
-
-}
 
 
 function toggleComplete(){
@@ -21,5 +24,15 @@ function toggleComplete(){
   lastComplete.classList.toggle('complete');
 }
 
-toggleButton.onclick = toggleComplete;
-document.addEventListener("change", add());
+// tasks.onclick = add;
+
+
+// toggleButton.onclick = toggleComplete;
+// document.addEventListener("change", add());
+
+checkBox.onchange = function(){
+  if (taskDetails == journeyDetails) {
+
+  }
+
+};
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
     @user = current_user
     @user.update_attributes(user_params)
     @services = Service.all
-    redirect_to services_path(current_user, query: current_user.path_type)
+    redirect_to dashboard_path
+    # redirect_to services_path(current_user, query: current_user.path_type)
   end
 
   def edit
