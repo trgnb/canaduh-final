2f23b26619dd3833de8fa8a4f77324e206f04b77
rosemarie betrong <rmbt.lab@gmail.com>
2020-04-02 18:21:08 -0400

    changed datepicker for rides

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,10 +2,14 @@ import flatpickr from "flatpickr"
 // import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import "flatpickr/dist/themes/material_red.css"
 
+// Appointment Calendar //
 flatpickr(".datepicker",
   {
     enableTime: true,
     dateFormat: "Y-m-d H:i",
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
     minTime: "09:00",
     maxTime: "17:00",
     inline: true,
@@ -14,7 +18,18 @@ flatpickr(".datepicker",
   }
 )
 
+// Milestones Calendar //
 flatpickr(".datepicker1",
+  {
+    dateFormat: "Y-m-d H:i",
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+  }
+)
+
+// Rides Calendar //
+flatpickr(".datepicker2",
   {
     dateFormat: "Y-m-d H:i",
     altInput: true,
