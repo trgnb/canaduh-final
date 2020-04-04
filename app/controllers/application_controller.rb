6454c2293442d52abab98cc12207a6c73e0c8122
mickael merle <mickaelmerle6901@gmail.com>
2020-04-03 22:00:50 -0400

    resolving conflicts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :location, :user_type, :user_bio, :specialty])
@@ -12,5 +16,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(user)
     root_path
+  end
+
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 end
